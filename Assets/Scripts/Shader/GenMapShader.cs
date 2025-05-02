cdc89f70b49ae6fd2dc54bc2e6ccda53c2d5816a
narekb8 <nab176@pitt.edu>
2025-05-01 23:54:12 -0400

    Created a simple demo scene

--- Assets/Scripts/Shader/GenMapShader.cs
+++ Assets/Scripts/Shader/GenMapShader.cs
@@ -1,3 +1,6 @@
+using System.Collections;
+using System.Threading.Tasks;
+using TMPro;
 using UnityEngine;
 using UnityEngine.Experimental.Rendering;
 using UnityEngine.Rendering;
@@ -10,7 +13,15 @@ public class GenMapShader : MonoBehaviour
     [SerializeField] Vector3 mapSize = new Vector3(16, 16, 16);
     [SerializeField] bool useRand = true;
     [SerializeField] int arenaSeed = 10;
-    [SerializeField] bool useSmoothNormals = true;
+    [SerializeField] public bool useSmoothNormals = true;
+    [SerializeField] public bool useCubes = true;
+    [SerializeField] TMP_Text normalsText;
+    [SerializeField] TMP_Text algoText;
+    [SerializeField] TMP_Text vertsText;
+    [SerializeField] TMP_Text trisText;
+    private float _Dissolve = 2f;
+    private GameObject m1;
+    private GameObject m2;
 
     struct Triangle
     {
@@ -30,12 +41,39 @@ public class GenMapShader : MonoBehaviour
     RenderTexture _weight;
 
     [SerializeField] ComputeShader MarchingShader;
+    [SerializeField] ComputeShader CubesShader;
+    [SerializeField] ComputeShader TetraShader;
     [SerializeField] ComputeShader NoiseShader;
 
-    private void Awake()
+    async Awaitable Awake()
     {
         //mapSize *= resolution;
+        MarchingShader = useCubes ? CubesShader : TetraShader;
+        if(useRand) arenaSeed = Random.Range(int.MinValue, int.MaxValue);
+        Random.InitState(arenaSeed);
+
         CreateBuffers();
+        await GenerateFirstMesh();
+    }
+
+    public void UpdateNormals(bool smooth)
+    {
+        useSmoothNormals = smooth;
+
+    }
+
+    public void UpdateAlgo(float val)
+    {
+        if(val > 0)
+        {
+            useCubes = false;
+            algoText.text = "Tetrahedra";
+        }
+        else
+        {
+            useCubes = true;
+            algoText.text = "Cubes";
+        }
     }
 
     void CreateBuffers()
@@ -56,22 +94,22 @@ public class GenMapShader : MonoBehaviour
     }
   
     // Start is called before the first frame update
-    void Start()
+    async Awaitable GenerateFirstMesh()
     {
-        if(useRand) arenaSeed = UnityEngine.Random.Range(int.MinValue, int.MaxValue);
-        UnityEngine.Random.InitState(arenaSeed);
-        Debug.Log(arenaSeed);
-        arenaSeed = UnityEngine.Random.Range(0, 1000000);
+        arenaSeed = Random.Range(0, 1000000);
         
-        Mesh mesh = ConstructMesh();
+        Mesh mesh = await ConstructMesh();
+
+        //while(_Dissolve < mapSize.y * resolution)
+            //await Awaitable.NextFrameAsync();
         
-        GameObject m1 = new GameObject();
+        m1 = new GameObject();
         m1.AddComponent<MeshFilter>().sharedMesh = mesh;
         m1.AddComponent<MeshRenderer>().materials = gameObject.GetComponent<MeshRenderer>().materials;
         m1.GetComponent<MeshRenderer>().shadowCastingMode = ShadowCastingMode.TwoSided;
         m1.transform.localScale *= resolution;
     
-        GameObject m2 = new GameObject();
+        m2 = new GameObject();
         m2.AddComponent<MeshFilter>().sharedMesh = mesh;
         m2.AddComponent<MeshRenderer>().materials = gameObject.GetComponent<MeshRenderer>().materials;
         m2.GetComponent<MeshRenderer>().shadowCastingMode = ShadowCastingMode.TwoSided;
@@ -79,6 +117,43 @@ public class GenMapShader : MonoBehaviour
 
         m1.AddComponent<MeshCollider>().sharedMesh = mesh;
         m2.AddComponent<MeshCollider>().sharedMesh = mesh;
+
+        StopAllCoroutines();
+        StartCoroutine(DissolveMesh(false));
+    }
+
+    async Awaitable GenerateNewMesh()
+    {
+        ClearBuffers();
+        CreateBuffers();
+
+        arenaSeed = Random.Range(0, 1000000);
+        
+        Mesh mesh = await ConstructMesh();
+
+        while(_Dissolve < mapSize.y * resolution)
+            await Awaitable.NextFrameAsync();
+
+        Destroy(m1);
+        Destroy(m2);
+        
+        m1 = new GameObject();
+        m1.AddComponent<MeshFilter>().sharedMesh = mesh;
+        m1.AddComponent<MeshRenderer>().materials = gameObject.GetComponent<MeshRenderer>().materials;
+        m1.GetComponent<MeshRenderer>().shadowCastingMode = ShadowCastingMode.TwoSided;
+        m1.transform.localScale *= resolution;
+    
+        m2 = new GameObject();
+        m2.AddComponent<MeshFilter>().sharedMesh = mesh;
+        m2.AddComponent<MeshRenderer>().materials = gameObject.GetComponent<MeshRenderer>().materials;
+        m2.GetComponent<MeshRenderer>().shadowCastingMode = ShadowCastingMode.TwoSided;
+        m2.transform.localScale = new Vector3(resolution, resolution, -resolution);
+
+        m1.AddComponent<MeshCollider>().sharedMesh = mesh;
+        m2.AddComponent<MeshCollider>().sharedMesh = mesh;
+
+        StopAllCoroutines();
+        StartCoroutine(DissolveMesh(false));
     }
 
     private void OnDestroy()
@@ -107,7 +182,7 @@ public class GenMapShader : MonoBehaviour
         return triCount[0];
     }
 
-    Mesh ConstructMesh()
+    async Awaitable<Mesh> ConstructMesh()
     {
         NoiseShader.SetTexture(0, "_Weights", _weight);
         MarchingShader.SetTexture(0, "_Weights", _weight);
@@ -120,7 +195,7 @@ public class GenMapShader : MonoBehaviour
         NoiseShader.Dispatch(0, (int)mapSize.x / 8, (int)mapSize.y / 8, (int)mapSize.z / 8);
         
         MarchingShader.SetBuffer(0, "_Triangles", _triBuffer);
-	MarchingShader.SetBool("_SmoothNormals", useSmoothNormals);
+	    MarchingShader.SetBool("_SmoothNormals", useSmoothNormals);
         MarchingShader.SetFloat("_ChunkSizeX", mapSize.x);
         MarchingShader.SetFloat("_ChunkSizeY", mapSize.y);
         MarchingShader.SetFloat("_ChunkSizeZ", mapSize.z);
@@ -130,10 +205,14 @@ public class GenMapShader : MonoBehaviour
 
         MarchingShader.Dispatch(0, (int)mapSize.x / 8, (int)mapSize.y / 8, (int)mapSize.z);
 
+        AsyncGPUReadbackRequest readback = AsyncGPUReadback.Request(_triBuffer, _triBuffer.count, 0);
+
+        while(!readback.done)
+            await Awaitable.NextFrameAsync();
+
         Triangle[] triangles = new Triangle[ReadTriangleCount()];
         _triBuffer.GetData(triangles);
 
-        //SaveRT3DToTexture3DAsset(_weight, "Debug/TestFile");
         return CreateMeshFromTriangles(triangles);
     }
 
@@ -165,9 +244,44 @@ public class GenMapShader : MonoBehaviour
         {
             indexFormat = IndexFormat.UInt32
         };
+
+        vertsText.text = "Vertex Count: " + verts.Length;
+        trisText.text = "Tri Count: " + triangles.Length;
         mesh.SetVertices(verts);
         mesh.SetNormals(normals);
         mesh.SetTriangles(tris, 0, true);
         return mesh;
-    }    
+    }
+
+    public async void NewMesh()
+    {
+        MarchingShader = useCubes ? CubesShader : TetraShader;
+        StopAllCoroutines();
+        StartCoroutine(DissolveMesh(true));
+
+        await GenerateNewMesh();
+    }
+
+    public IEnumerator DissolveMesh(bool dissolve)
+    {
+        MeshRenderer arenaMat = gameObject.GetComponent<MeshRenderer>();
+        if(dissolve)
+        {
+            while(_Dissolve < mapSize.y * resolution)
+            {
+                _Dissolve += mapSize.y / 3f * Time.deltaTime * resolution;
+                arenaMat.sharedMaterial.SetFloat("_DissolveAmount", _Dissolve);
+                yield return null;
+            }
+        }
+        else
+        {
+            while(_Dissolve > -0.8f)
+            {
+                _Dissolve -= mapSize.y / 3f * Time.deltaTime * resolution;
+                arenaMat.sharedMaterial.SetFloat("_DissolveAmount", _Dissolve);
+                yield return null;
+            }
+        }
+    }
 }
